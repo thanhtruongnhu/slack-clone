2c19530df6ee7c4e6eb7c4ff861b6270ce80e7a9
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-02 09:07:07 -0400

    update on header and sidebar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
   return (
     // BEM naming convention
     <div className="App">
-      <h1>Hello World</h1>
 
       {/*Header*/} 
       <Header/>
+
       {/*Sidebar*/}
       {/*React-Router -> Switching btw channels without refreshing*/ }
+      <div className="app_body">
+        <Sidebar />
+      </div>
+
     </div>
   );
 }
