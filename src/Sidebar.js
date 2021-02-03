4ed0310b30d61c84acd7b021fab3ec0172fe34e9
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-03 09:25:15 -0400

    Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Sidebar.css'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+import InsertCommentIcon from '@material-ui/icons/InsertComment'; 
+import SidebarOption from './SidebarOption'
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__info">
+                    <h2> Main Board</h2>
+                    <h3> 
+                        <FiberManualRecordIcon />
+                        Kyle Nhu
+                        
+                    </h3>
+                </div>
+                <CreateIcon />
+                
+            </div>
+            <SidebarOption Icon={InsertCommentIcon} title="Threads" /> {/*Use capital "I" in Icon becuz, we passing a component as a prop*/ }
+            <SidebarOption title="Youtube" />
         </div>
     )
 }
---
+++ src/SidebarOption.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './SidebarOption.css'
+
+function SidebarOption({ Icon, title}) {
+    return (
+        <div className="sidebarOption">
+
+            {/*ICON*/}
+            {Icon && <Icon className="sidebarOption__icon"/>} {/*Only render an icon when Icon is passed in (available)*/ }
+
+
+            {/*TEXT*/}
+            {Icon ? (<h3>{title}</h3>): (
+                <h3>
+                    <span className="sidebarOption__hash">#</span> {title}
+                </h3>
+            )
+            } {/*If you don't pass an icon, it will start a channel (start with #) */}
+        </div>
+    )
+}
+
+export default SidebarOption
