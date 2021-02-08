1b69134471c64efc63b318e8095193ea94c330be
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-02-07 21:57:41 -0400

    misc update

--- src/Message.js
+++ src/Message.js
@@ -1,3 +1,4 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react'
 import  "./Message.css";
 
@@ -6,7 +7,12 @@ function Message({ message, timestamp, user, userImage }) {
 
     return (
         <div className="message">
-            <img src={userImage} alt=""/>
+            {/* <img src={userImage} alt=""/> */}
+            <Avatar 
+                className="message__avatar"
+                    alt={user?.displayName}
+                    src={userImage}
+                />
             <div className="message__info">
                 <h4>{user} <span className='message__timestamp'>{new Date(timestamp?.toDate()).toUTCString()}</span></h4>
                 <p>{message}</p>
